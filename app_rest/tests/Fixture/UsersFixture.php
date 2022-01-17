43f14a97df4d36e90223a90bf264f0c6267d4754
Alex <alex.gomez1997@hotmail.com>
2022-01-17 13:03:14 +0100

    Added password hasher

--- app_rest/tests/Fixture/UsersFixture.php
+++ app_rest/tests/Fixture/UsersFixture.php
@@ -1,17 +1,29 @@
 <?php
+
 namespace App\Test\Fixture;
 
 use Cake\TestSuite\Fixture\TestFixture;
 
 class UsersFixture extends TestFixture
 {
+    public $fields = [
+        'id' => ['type' => 'integer'],
+        'email' => ['type' => 'string', 'null' => false],
+        'firstname' => ['type' => 'string', 'null' => false],
+        'lastname' => ['type' => 'string', 'null' => false],
+        'password' => ['type' => 'string'],
+        'group_id' => ['type' => 'integer'],
+        'created' => ['type' => 'datetime'],
+        'modified' => ['type' => 'datetime']
+    ];
+
     public $records = [
         [
             'id' => 1,
             'email' => 'test@example.com',
             'firstname' => 'My Name',
             'lastname' => 'My Surname',
-            'password' => '$2a$10$5XHCbLVU4Z15o3tSsn6BD.vRO6zmSk34kj2lkjsdklfjwiejo2lke',
+            'password' => '$2y$10$1cCayk8qquFFWyvk161qZuOm4kgLFbmg4O1ItVQ5Qt.w3V28VNUk2',
             'group_id' => 3,
             'created' => '2021-01-18 10:39:23',
             'modified' => '2021-01-18 10:41:31'
