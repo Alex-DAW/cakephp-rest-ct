8f6f7552771cbb320ded0d1d9a09a0701192926c
Alex <alex.gomez1997@hotmail.com>
2022-01-17 11:20:04 +0100

    Api2UsersControllerTest added Jsondecode from the the response

---
+++ app_rest/src/Controller/Api2AuthenticationController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Entity\User;
+use App\Model\Table\UsersTable;
+
+/**
+ * @property UsersTable $Users
+ */
+class Api2AuthenticationController extends Api2Controller
+{
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->Users = UsersTable::load();
+    }
+
+    public function isPublicController(): bool
+    {
+        return true;
+    }
+
+    protected function getMandatoryParams(): array
+    {
+        return [];
+    }
+
+    protected function addNew($data)
+    {
+        /** @var User $user */
+        $this->return = ['access_token' => 'sdfgsfg'];
+    }
+
+}
