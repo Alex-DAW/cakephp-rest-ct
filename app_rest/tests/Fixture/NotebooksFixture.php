c781681bd9fc4401db7dc99403034b17eabd6f44
Alex <alex.gomez1997@hotmail.com>
2022-01-19 10:56:38 +0100

    Created the fixture from Notebooks

---
+++ app_rest/tests/Fixture/NotebooksFixture.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+class NotebooksFixture extends TestFixture
+{
+    public $records = [
+        [
+            'id' => 1,
+            'user_id' => 1,
+            'title' => 'Título 1',
+            'shape' => 'TODO',
+            'created' => '2021-01-18 10:39:23',
+            'modified' => '2021-01-18 10:41:31'
+        ],
+    ];
+}
