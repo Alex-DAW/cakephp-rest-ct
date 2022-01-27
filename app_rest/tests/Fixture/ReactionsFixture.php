28494c9325bc2a43ce34962ce5896dbe2ba1d02b
Alex <alex.gomez1997@hotmail.com>
2022-01-27 11:22:49 +0100

    Delete user_id column and change emoji data

--- app_rest/tests/Fixture/ReactionsFixture.php
+++ app_rest/tests/Fixture/ReactionsFixture.php
@@ -9,9 +9,8 @@ class ReactionsFixture extends TestFixture
     public $records = [
         [
             'id' => 1,
-            'user_id'=>1,
             'note_id' => 1,
-            'emoji' => 'Note 1',
+            'emoji' => ':D',
             'created' => '2021-01-18 10:39:23',
             'modified' => '2021-01-18 10:41:31'
         ]
